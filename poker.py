9daa13ddb02adc46536cc1b05a4777410bbf5349
cih831 <choi83195@gmail.com>
2022-05-03 17:13:36 +0900

    first commit

---
+++ poker.py
@@ -0,0 +1,39 @@
+def hand_rankings():
+    rank = ['top', 'one_pair', 'two_pair', 'triple', 'straight', 'flush', 'full_house', 'straight_flush', 'loyal_straight_flush']
+
+def get_card(cards):
+    global s, d, h, c
+    for card in cards:
+        num, sym = map(str, card)
+        num = int(num)
+        if sym == 's':
+            s[num] += 1
+        elif sym == 'd':
+            d[num] += 1
+        elif sym == 'h':
+            h[num] += 1
+        else:
+            c[num] += 1
+
+c = [[]*14]
+
+hero_hands = list(map(str, input().split()))
+get_card(hero_hands)
+# print(s, d, h, c)
+
+flop = list(map(str, input().split()))
+get_card(flop)
+# print(s, d, h, c)
+
+turn = [input()]
+print(turn)
+get_card(turn)
+# print(s, d, h, c)
+
+river = [input()]
+print(river)
+get_card(river)
+
+# 족보 함수
+# 게임 진행
+# ai
